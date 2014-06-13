7d4ad6ccbe1fa10a20c8e0e9f0a4b3282d90051e
snakeek <skywalker124@gmail.com>
2014-06-13 22:49:51 +0800

    add base files
    hello world about node.js

---
+++ lesson01/HelloWorld.js
@@ -0,0 +1,3 @@
+console.log('Hello World!');
+console.log('NBServer');
+console.log("TEST");
---
+++ lesson01/HttpServer/simpleHttpServer.js
@@ -0,0 +1,10 @@
+/**
+ * Created by snakeek on 2014/6/9.
+ */
+var http = require('http');
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.write('<h1>Node.js</h1>');
+    res.end('<p>Hello World</p>');
+}).listen(3000);
+console.log("Http Server is listening at port 3000");
