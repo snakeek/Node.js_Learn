fa942bc0d2d0e105af312134da6f574ec0f59909
snakeek <skywalker124@gmail.com>
2014-08-29 16:09:06 +0800

    node.js base over

--- lesson01/HttpServer/Server.js
+++ lesson01/HttpServer/Server.js
@@ -2,10 +2,31 @@
  * Created by snakeek on 2014/8/27.
  */
 var http = require('http');
+var url = require('url');
+var util = require('util');
+var querystring = require('querystring');
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
+//Get Request
+/*
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(util.inspect(url.parse(req.url, true)));
+}).listen(8888);
+*/
+
+//Post Request
+http.createServer(function(req, res) {
+    var post = '';
+
+    req.on('data', function(chunk) {
+        post += chunk;
+    });
+
+    req.on('end', function() {
+        post = querystring.parse(post);
+        console.log(util.inspect(post));
+        res.end(util.inspect(post));
+    });
 }).listen(8888);
 
-console.log('Server running at http://127.0.0.1:8888/');
+console.log('Server running on port 8888.');
